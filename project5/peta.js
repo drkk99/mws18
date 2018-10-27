daf33b06011c110e8c74cecc069ad1ca43822c9c
Dian Rizqi Khusnul Khotimah <16.9079@stis.ac.id>
2018-10-28 06:47:56 +0700

    edit peta.js

--- project5/peta.js
+++ project5/peta.js
@@ -13,23 +13,27 @@ function findLocation(x,y) {
 	//console.log("you clicked " + e.latlng.lat + " dan "+ e.latlng.lng);    
 	let ix= findLocation(e.latlng.lat,e.latlng.lng);   
 	if (ix >=0) {        
-		img.src= places[ix].gambar;        
+		img.src= places[ix].gambar;  
+		img.alt = places[ix].title;
 		par.textContent=places[ix].review;
 		} 
 	} 
 	
-	(async function (){  
-		const URL="../project5/peta.json";
-		try {        
-			let resp= await(fetch(URL));
-			let resp2= await resp.json();        
-			localStorage.setItem('places',             
-				JSON.stringify(resp2.places));//    
-		}    
-		catch(err){        
-			console.log(err);    
-		} 
-	})( ); 
+	fetch(URL)
+    .then(function(response){
+        if (response.status !== 200) {
+            console.log('There is a problem . Status Code: ' + response.status);
+            throw response.statusText;
+        }
+        return response.json()
+    })
+    .then ( resp => {
+        localStorage.setItem('places', JSON.stringify(resp.places));
+        setView();
+    })
+    .catch(function(err){
+        console.log(err);
+    });
 	
 	let gmb= document.getElementById("gmb"); 
 	let rev= document.getElementById("review"); 
